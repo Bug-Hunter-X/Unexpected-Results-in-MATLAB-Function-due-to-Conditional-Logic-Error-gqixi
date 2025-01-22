9854132e270f898f8af44da982247df3795b17bb
BugHunter-X <askzenai@gmail.com>
2025-01-22 18:38:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,13 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  ...
+  if someCondition
+    result = someValue; 
+  else
+    result = anotherValue;
+  end
+end
+
+% Example usage:
+input = 10;
+result = myFunction(input);
